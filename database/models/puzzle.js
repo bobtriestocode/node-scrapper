6904a511fc95ec7f7a263d3f243c3f15ceb3e9aa
Ratul <ratul.mukhopadhyay@kartocket.com>
2021-03-28 20:19:56 +0530

    changes

---
+++ database/models/puzzle.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+const PuzzleSchema = new Schema({
+    puzzle_url: {type: String,unique:true},
+    difficulty: {type: Number, default: Date.getDay},
+    created_at: {type: Date, default: Date.now}
+})
+
+module.exports = mongoose.model("Puzzle", PuzzleSchema, "puzzles");
--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const cheerio = require('cheerio')
 const axios = require('axios');
+const db = require('./database/db');
+let puzzle = db.Puzzle;
 const link = "https://www.chessgames.com";
-
 var start = async function(a, b) { 
     // Your async task will execute with await
     var scraped_data =await axios.get("https://www.chessgames.com");
     const $ = cheerio.load(scraped_data.data) 
     var text = $("table").first().css("background-color","#FFFFDD").find("center > font > a").attr('href');
     var text =link+text;
-    console.log(text);
+    var data = {
+        "puzzle_url" : text
+    }
+    try {
+        let testAddress = new puzzle(data);
+        let response = await testAddress.save();
+        console.log(response);
+    } catch (error) {
+        console.log("there are the catch error", error);
+    }
   }
-
 start();
+
+
 
