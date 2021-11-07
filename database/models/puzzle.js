342b56d4a1c4024a01ff8b4013cd8280c4465a75
Ratul Mukhopadhyay <letushelloworld@gmail.com>
2021-11-07 14:41:23 +0530

    Fix: Added difficulty column entry in DB

--- database/models/puzzle.js
+++ database/models/puzzle.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const PuzzleSchema = new Schema({
     puzzle_url: {type: String,unique:true},
-    difficulty: {type: Number, default: Date.getDay},
+    difficulty: {type: Number, default: new Date().getDay()},
     created_at: {type: Date, default: Date.now}
 })
 
